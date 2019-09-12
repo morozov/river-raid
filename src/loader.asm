a32f8f0d1f9f59401fc87e44ec59da37978b6898
Sergei Morozov <morozov@tut.by>
2019-09-11 19:14:46 -0700

    Clear the screen

--- src/loader.asm
+++ src/loader.asm
@@ -1,3 +1,11 @@
+; Clear the screen
+XOR A            ; BORDER 0
+OUT ($FE), A     ;
+LD HL, #4000     ; from 16385
+LD DE, #4001     ; to   16385
+LD BC, #1AFF     ; repeat 6911 times
+LDIR             ; copy
+
 ; Load the image
 LD DE, (#5CF4)   ; restore the FDD head position
 LD BC, #0705     ; load 7 sectors of compressed image
