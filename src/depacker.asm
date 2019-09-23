78c5e547e7b7497d20f1aabe152dbf6419dea2ca
Sergei Morozov <morozov@tut.by>
2019-09-23 00:08:35 -0700

    Restore system variables

--- src/depacker.asm
+++ src/depacker.asm
@@ -1,6 +1,7 @@
 pr_string   EQU #203C ; https://skoolkid.github.io/rom/asm/1FFC.html#203C
 chan_open   EQU #1601 ; https://skoolkid.github.io/rom/asm/1601.html
 dstbuffer   EQU #5B00
+dstsys      EQU #5C4F
 dstprog     EQU #5CCB
 dstlern     EQU #7800
 
@@ -23,6 +24,11 @@ srclern:
 at00:
     DEFB    #16,#00,#00 ; AT 0,0
 
+sysvars:
+    DEFB    #B6,#5C,#B6,#5C,#CB,#5C,#CB,#5C
+    DEFB    #00,#80,#EA,#5C,#00,#00,#EC,#5C
+    DEFB    #FD,#5C,#FD,#5C
+
 start:
     LD      HL, srcbuffer
     LD      DE, dstbuffer
@@ -36,11 +42,17 @@ start:
     LD      DE, dstlern
     CALL    dzx7_standard
 
+; Restore system variables
+    LD      HL, sysvars
+    LD      DE, dstsys
+    LD      BC, start-sysvars
+    LDIR
+
 ; PRINT AT 0,0
     LD      A, #02
     CALL    chan_open
     LD      DE,at00
-    LD      BC,start-at00
+    LD      BC,sysvars-at00
     CALL    pr_string
 
 ; RANDOMIZE USR 23762
