e89e7d3fb4a85b7c5f495d91f21e2738516d96bc
Sergei Morozov <morozov@tut.by>
2019-09-25 23:16:29 -0700

    Redefined keyboard controls

--- src/depacker.asm
+++ src/depacker.asm
@@ -4,6 +4,9 @@ dstbuffer   EQU #5B00
 chans       EQU #5C4F ; https://skoolkid.github.io/rom/asm/5C4F.html
 chinfo      EQU #5CB6 ; https://skoolkid.github.io/rom/asm/5CB6.html
 dstprog     EQU #5CCB
+kbdupdn     EQU #607A
+moveup      EQU #670A
+movedn      EQU #6717
 dstlern     EQU #7800
 
 ; it should be at least 63488−16687=46801 in order to not have
@@ -25,6 +28,21 @@ srclern:
 at00:
     DEFB    #16,#00,#00 ; AT 0,0
 
+; see http://www.breakintoprogram.co.uk/computers/zx-spectrum/keyboard
+kbdpatch:
+
+; Map Q instead of 2 to UP
+    LD      A, $FB
+    IN      A, ($FE)
+    BIT     0, A
+    CALL    Z, moveup
+
+; Map A instead of W to DOWN
+    LD      A, $FD
+    IN      A, ($FE)
+    BIT     0, A
+    CALL    Z, movedn
+
 start:
     LD      HL, srcbuffer
     LD      DE, dstbuffer
@@ -38,6 +56,12 @@ start:
     LD      DE, dstlern
     CALL    dzx7_standard
 
+; Patch keyboard controls
+    LD      HL, kbdpatch
+    LD      DE, kbdupdn
+    LD      BC, start - kbdpatch
+    LDIR
+
 ; Set CHANS back to its 48K value
     LD      HL, chans
     LD      BC, chinfo
@@ -49,7 +73,7 @@ start:
     LD      A, #02
     CALL    chan_open
     LD      DE,at00
-    LD      BC,start-at00
+    LD      BC,kbdpatch-at00
     CALL    pr_string
 
 ; RANDOMIZE USR 23762
